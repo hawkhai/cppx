19280e2125871141f02771316de67053ed5c0d72
haihoing <haihoing@qq.com>
2022-07-26 15:40:59 +0800

    E:\kSource\pythonx\cppx\QtConsoleApplication\cfcore

---
+++ QtConsoleApplication/cfcore/cfcore.cpp
@@ -0,0 +1,22 @@
+﻿// cfcore.cpp : 定义 DLL 的导出函数。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "cfcore.h"
+
+
+// 这是导出变量的一个示例
+CFCORE_API int ncfcore=0;
+
+// 这是导出函数的一个示例。
+CFCORE_API int fncfcore(void)
+{
+    return 0;
+}
+
+// 这是已导出类的构造函数。
+Ccfcore::Ccfcore()
+{
+    return;
+}
